13472fbaa6c762c67077bb75c7bea5c36408399c
CastleRun <786901a@gmail.com>
2015-06-03 16:23:58 -0700

    -

--- test.php
+++ test.php
@@ -1,9 +1,9 @@
-<!DOCTYPE html>
 <html>
   <body>
     <?php
-    #If you see this, PHP doesn't work on this server
-    echo "PHP works!";
+    echo "<h1>Kittens Screen Coming</h1>";
+    echo "I have just installed PHP on my server, but will start working on the Kittens screen, where you can get your kittens.<br>";
+    echo "Have your access code ready for when this screen will come up.<br>";
     ?>
   </body>
 </html>
