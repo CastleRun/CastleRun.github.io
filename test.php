56390adf1f79632da32506839e18d8561dc13f9d
CastleRun <786901a@gmail.com>
2015-10-12 14:27:06 -0700

    Website Cleanup

--- test.php
+++
@@ -1,9 +0,0 @@
-<html>
-  <body>
-    <?php
-    echo "<h1>Kittens Screen Coming</h1>";
-    echo "I have just installed PHP on my server, but will start working on the Kittens screen, where you can get your kittens.<br>";
-    echo "Have your access code ready for when this screen will come up.<br>";
-    ?>
-  </body>
-</html>
